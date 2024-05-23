6eb8f2dc63ebc476eccbb386020d9df6ff12825d
Anurag <shuklaanurag998@gmail.com>
2024-05-24 00:49:44 +0530

    Added navigation and improved UI

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,10 +1,32 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 
 export default function SignIn(){
+  const navigate = useNavigate();
+
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    useEffect(()=>{
+      axios.post("http://localhost:3000/api/v1/user/me", {}, {headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem("token")
+      }}).then((resp) => {
+        if(resp.data.msg == "Authenticated"){
+          navigate("/dashboard");
+        }
+        else{
+          console.log(resp.data.msg)
+          console.log("Not auth");
+        }
+      })
+
+      
+
+    }, [])
+
+    
 
 
     return (
@@ -26,20 +48,26 @@ export default function SignIn(){
           setPassword(e.target.value);
         }} type="password" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"></input></div>
         
-        <div className="mt-7 mb-7">
+        <div className="mt-7 mb-2">
         <button onClick = {async ()=> {
           const resp = await axios.post("http://localhost:3000/api/v1/user/signin", {
             username,
             password
           })
           if(resp.data.msg == "Signed In Successfully"){
-            console.log(resp.data.message);
+            console.log(resp.data.msg);
+            localStorage.setItem("token", resp.data.token);
+            navigate("/dashboard");
           }
         }} className="w-full text-white bg-gray-700 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"> Sign In</button>
         </div>
+
+        <div className="flex justify-center"> 
+        <div className="mr-2"> Don't have an account? </div>
+        <Link className="underline" to={"/signup"}> Sign Up </Link></div>
         </div>
         </div>
         </section>
       </>
-    )
+    );
   }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,24 @@
 import axios from "axios";
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom";
+
 
 export default function SignUp(){
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    axios.post("http://localhost:3000/api/v1/user/me", {}, {headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem("token")
+    }}).then((resp) => {
+      if(resp.data.msg == "Authenticated"){
+        navigate("/dashboard");
+      }
+      else{
+        console.log(resp.data.msg)
+        console.log("Not auth");
+      }
+    })
+  }, [])
 
   const [userName, setUserName] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -47,12 +64,17 @@ export default function SignUp(){
                 });
                 console.log(resp.data);
                 if(resp.data.msg == "User created successfully"){
-                  localStorage.setItem("token", resp.data.token)
+                  localStorage.setItem("token", resp.data.token);
+                  navigate("/dashboard");
                 }
                 else{
                   console.log("Failed to sign up");
                 }
               }} className="w-full text-white bg-gray-700 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"> Sign Up</button>
+          </div>
+          <div className="flex justify-center "> 
+          <div className="mr-2"> Already have an account? </div>
+          <Link className="underline" to={"/signin"}> Sign In </Link>
           </div>
           </div>
         </div>
@@ -60,6 +82,5 @@ export default function SignUp(){
 
       </>
     )
-
 
   }
